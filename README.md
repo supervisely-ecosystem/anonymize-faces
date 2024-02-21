d9f889062889ebb761ad21a1aab44e7d73500d49
Nikolai Petukhov <petukhov.niko@gmail.com>
2024-02-21 20:06:57 -0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# app-template-headless
+
+template for your app without GUI. This app prints progress bar 10 times and sleeps 1 second on each iteration. That's it!
+
+See video how app works:
+
+<a href="https://www.youtube.com/watch?v=4VdvP0SRbiM" title="Video" target="_blank">
+  <img src="https://i.imgur.com/XO1LGw9.png" alt="Watch video"/>
+</a>
---
+++ src/main.py
@@ -0,0 +1,45 @@
+import time
+
+import supervisely as sly
+
+import globals as g
+
+my_app = sly.AppService()
+
+
+@my_app.callback("do_something")
+@sly.timeit
+def do(api: sly.Api, task_id, context, state, app_logger):
+    app_logger.info("do something here")
+    # or just print (it is recommended to use app_logger)
+    # print("do something here")
+
+    steps_count = 10
+    task_progress = sly.Progress("Processing...", steps_count)
+
+    for i in range(steps_count):
+        print(i)
+        time.sleep(1)
+        task_progress.iter_done_report()
+
+    my_app.stop()
+
+
+def main():
+    sly.logger.info(
+        "Input arguments",
+        extra={
+            "TASK_ID": g.STATE.task_id,
+            "context.teamId": g.STATE.selected_team,
+            "context.workspaceId": g.STATE.selected_workspace,
+        },
+    )
+
+    initial_events = [{"state": None, "context": None, "command": "do_something"}]
+
+    # Run application service
+    my_app.run(initial_events=initial_events)
+
+
+if __name__ == "__main__":
+    sly.main_wrapper("main", main)
